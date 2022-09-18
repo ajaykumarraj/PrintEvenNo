92683d2c366af057abfad2945893a411c63a5082
ajaykumarraj <ajaysanjuraj@gmail.com>
2022-09-18 18:09:56 +0530

    This is for chaking prime no

---
+++ newcompany/TestPrimeNo.java
@@ -0,0 +1,24 @@
+package newcompany;
+import java.util.Scanner;
+
+public class TestPrimeNo {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Please enter a number of chaking prime or not");
+		int num = sc.nextInt();
+		int i=2;
+		while(i<num) {
+			if(num%i==0) {
+				System.out.println(num+" is not a prime no");
+				break;
+			}
+			i++;
+		}
+		if(num==i) {
+			System.out.println(num+" is a prime no");
+		}
+		
+	}
+
+}
