e6cd683d60072b2a0ce2b999877e7c25fd6812dc
ajaykumarraj <ajaysanjuraj@gmail.com>
2022-09-18 16:57:10 +0530

    I am printing 10 even no

---
+++ newcompany/PrintTenEvenNo.java
@@ -0,0 +1,14 @@
+package newcompany;
+
+public class PrintTenEvenNo {
+
+	public static void main(String[] args) {
+		System.out.println("This is your 10 even no");
+		for(int i=1;i<=20;i++) {
+			if(i%2==0) {
+				System.out.println(i);
+			}
+		}
+	}
+
+}
